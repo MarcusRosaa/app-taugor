02ebf3c80f34e831934ee57a130f841ab2c0b241
MarcusRosaa <marcusvrosa1@gmail.com>
2022-01-28 17:52:50 -0300

    remove log

--- src/Routes.js
+++ src/Routes.js
@@ -13,7 +13,6 @@ export default function Routes() {
 
   useEffect(() => {
     const storeUserState = localStorage.getItem('user');
-    console.log(storeUserState);
     if (storeUserState) {
       JSON.parse(storeUserState);
       setUser(true);
